18d39c003b25c142a06442f037e4391d967849a4
Daniel Roberson <daniel@planethacker.net>
2018-05-25 08:50:33 -0700

    added function descriptions

--- ELFcrypt.h
+++ ELFcrypt.h
@@ -33,32 +33,62 @@ void fatal(char *fmt, ...) {
 }
 
 
-/* get_elf_size() -- returns length of ELF data for a file.
+/* is_valid_elf() -- Determine if data is a valid ELF file.
  *
- * TODO: validate ELF file
- *       figure out actual header size of ELF (malloc)
+ * Args:
+ *     header - ELF header.
+ *
+ * Returns:
+ *     1 if ELF magic checks are successful.
+ *     0 if the magic bytes do not match.
+ */
+int is_valid_elf(Elf64_Ehdr *header) {
+  if (!header)
+    return 0;
+
+  if (header->e_ident[EI_MAG0] != ELFMAG0 ||
+      header->e_ident[EI_MAG1] != ELFMAG1 ||
+      header->e_ident[EI_MAG2] != ELFMAG2 ||
+      header->e_ident[EI_MAG3] != ELFMAG3)
+    return 0;
+
+  return 1;
+}
+
+
+/* get_elf_size() -- Calculate size of ELF data within a file.
+ *
+ * Args:
+ *     progname - ELF file
+ *
+ * Returns:
+ *     size of ELF data if successful.
+ *     -1 if an error occurred.
+ *
+ * Note: if an error occurs, errno is set appropriately.
  */
 size_t get_elf_size(const char *progname) {
-  int fd;
-  void *ELFheaderdata;
-  Elf64_Ehdr *ELFheader;
-  size_t elfsize;
+  int           fd;
+  void          *ELFheaderdata;
+  Elf64_Ehdr    *ELFheader;
+  size_t        elfsize;
 
 
   ELFheaderdata = malloc(64);
 
   fd = open(progname, O_RDONLY);
   if (fd == -1) {
-    fprintf(stderr, "Failed to open input file %s: %s\n",
-            progname,
-            strerror(errno));
-    fprintf(stderr, "Exiting.\n");
-
-    exit(EXIT_FAILURE);
+    errno = ENOENT;
+    return -1;
   }
 
   read(fd, ELFheaderdata, 64);
   ELFheader = (Elf64_Ehdr *)ELFheaderdata;
+
+  if (is_valid_elf(ELFheader) == 0) {
+    errno = ENOEXEC;
+    return -1;
+  }
 
   elfsize = ELFheader->e_shoff + (ELFheader->e_shnum * ELFheader->e_shentsize);
 
@@ -69,27 +99,20 @@ size_t get_elf_size(const char *progname) {
 }
 
 
-/* is_valid_elf() -- returns 0 if ELF header is valid, 1 if not
- */
-int is_valid_elf(Elf64_Ehdr *header) {
-  if (!header)
-    return 1;
-
-  if (header->e_ident[EI_MAG0] != ELFMAG0 ||
-      header->e_ident[EI_MAG1] != ELFMAG1 ||
-      header->e_ident[EI_MAG2] != ELFMAG2 ||
-      header->e_ident[EI_MAG3] != ELFMAG3)
-    return 1;
-
-  return 0;
-}
-
-
-/* get_elf_section()
+/* get_elf_section() -- Get address of ELF section within data.
+ *
+ * Args:
+ *     data    - ELF data in memory
+ *     section - section name to search for
+ *
+ * Returns:
+ *     Address of section if successful.
+ *     NULL if section does not exist.
  */
 Elf64_Shdr *get_elf_section(void *data, const char *section) {
-  int i;
-  char *offset;
+  int           i;
+  char          *offset;
+
 
   /* Populate ELF headers and section headers from mmapped file */
   Elf64_Ehdr *ELFheader = (Elf64_Ehdr *)data;
@@ -97,12 +120,8 @@ Elf64_Shdr *get_elf_section(void *data, const char *section) {
   Elf64_Shdr *next = &sectionheader[ELFheader->e_shstrndx];
 
   /* Make sure this is a valid ELF before proceeding */
-  if (is_valid_elf(ELFheader) == 1) {
-    fprintf(stderr, "Input file is not a valid ELF file.\n");
-    fprintf(stderr, "Exiting.\n");
-
-    exit(EXIT_FAILURE);
-  }
+  if (is_valid_elf(ELFheader) == 0)
+    fatal("Input file is not a valid ELF file.\n");
 
   offset = data + next->sh_offset;
 
@@ -117,10 +136,17 @@ Elf64_Shdr *get_elf_section(void *data, const char *section) {
 }
 
 
-/* get_file_size() -- returns size of file in bytes
+/* get_file_size() -- Determine the size of a file.
+ *
+ * Args:
+ *     filename - Path to file.
+ *
+ * Returns:
+ *     Size of file in bytes on success.
+ *     -1 if something went wrong.
  */
 size_t get_file_size(const char *filename) {
-  struct stat s;
+  struct stat     s;
 
   if (stat(filename, &s) == -1) {
     fprintf(stderr, "Failed to stat file %s: %s\n", filename, strerror(errno));
@@ -131,10 +157,21 @@ size_t get_file_size(const char *filename) {
 }
 
 
+/* get_password() -- Prompt user for a password and password confirmation.
+ *
+ * Args:
+ *     None
+ *
+ * Returns:
+ *     Pointer to string containing the password.
+ *
+ * Note: this is limited to 256 byte passwords because this is the maximum
+ * key length RC4 is able to use.
+ */
 char *get_password() {
-  int i = 0;
-  char *key;
-  char keyconfirm[256];
+  int             i = 0;
+  char            *key;
+  char            keyconfirm[256];
 
   do {
     if (i) {
@@ -164,12 +201,23 @@ char *get_password() {
 }
 
 
+/* rc4() -- Encrypt data using RC4 encryption algorithm.
+ *
+ * Args:
+ *     data - Data to encrypt
+ *     size - Length of data
+ *     key  - Passphrase to encrypt data with.
+ *
+ * Returns:
+ *     0 if successful.
+ *     1 if unsuccessful.
+ */
 int rc4(unsigned char *data, size_t size, const unsigned char *key) {
-  int i;
-  int rc4i;
-  int rc4j;
+  int           i;
+  int           rc4i;
+  int           rc4j;
   unsigned char rc4s[256];
-  unsigned int tmp;
+  unsigned int  tmp;
 
   if (strlen((char *)key) > sizeof(rc4s)) {
     fprintf(stderr, "Key must be under %ld bytes\n", sizeof(rc4s));
@@ -207,22 +255,30 @@ int rc4(unsigned char *data, size_t size, const unsigned char *key) {
 }
 
 
+/* ELFdecrypt() -- Decrypt .crypted section of ELF file.
+ *
+ * Args:
+ *     None
+ *
+ * Returns:
+ *     Nothing
+ */
 void ELFdecrypt() {
-  int section_length;
-  int crypted_section;
-  char *key;
+  int           section_length;
+  int           crypted_section;
+  char          *key;
   unsigned char *ptr;
   unsigned char *ptr2;
-  size_t pagesize;
-  uintptr_t pagestart;
-  int size;
+  size_t        pagesize;
+  uintptr_t     pagestart;
+  int           size;
 
 
   key = getenv("ELFCRYPT");
   if (key == NULL)
     key = getpass("Enter passphrase: ");
 
-  /* retrieve crypted section offset and size stored by ELFcrypt */
+  /* Retrieve crypted section offset and size stored by ELFcrypt */
   crypted_section = *((int *)(ENTRY + 0x09));
   section_length = *((short *)(ENTRY + 0x0d));
 
@@ -233,21 +289,16 @@ void ELFdecrypt() {
   pagestart = (uintptr_t)ptr & -pagesize;
   size = (ptr2 - (unsigned char *)pagestart);
 
-  if (mprotect((void *)pagestart, size, PROT_READ | PROT_WRITE | PROT_EXEC) == -1) {
-    fprintf(stderr, "mprotect(): %s\n", strerror(errno));
-    fprintf(stderr, "Exiting.\n");
-    exit(EXIT_FAILURE);
-  }
+  if (mprotect((void *)pagestart, size, PROT_READ | PROT_WRITE | PROT_EXEC) == -1)
+    fatal("mprotect(): %s\n", strerror(errno));
 
   /* decrypt using specified key */
   rc4(ENTRY + crypted_section, section_length, (unsigned char *)key);
 
-  if (mprotect((void *)pagestart, size, PROT_READ | PROT_EXEC) < 0) {
-    fprintf(stderr, "mprotect2\n");
-    fprintf(stderr, "Exiting.\n");
-    exit(EXIT_FAILURE);
-  }
+  if (mprotect((void *)pagestart, size, PROT_READ | PROT_EXEC) < 0)
+    fatal("mprotect(): %s\n", strerror(errno));
 
   /* erase key */
   memset(key, 0, strlen(key));
 }
+
